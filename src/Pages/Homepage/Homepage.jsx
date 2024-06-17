0796631f910fcf107e0e33f46859016445d39f20
Barshon <barshonweb@gmail.com>
2024-06-17 12:48:55 +0600

    notiflix alerts

--- src/Pages/Homepage/Homepage.jsx
+++ src/Pages/Homepage/Homepage.jsx
@@ -1,11 +1,15 @@
 import { Toaster, toast } from "sonner";
+import Notiflix from "notiflix";
 
 const Homepage = () => {
     return (
         <div>
             <p>Homepage</p>
             <Toaster></Toaster>
-            <button onClick={()=>toast('hello!')} className="btn">toast</button>
+            <button onClick={()=>{toast('hello!');
+                Notiflix.Notify.success('Sol lucet omnibus');
+
+            }} className="btn">toast</button>
         </div>
     );
 };
